667bc9801a9a3b02754c644176648ec9ed5276bd
Tilen Tinta <titi.tinta1@gmail.com>
2025-01-30 19:18:58 +0100

    Update: added option to specify the updating brench. version

--- Code/main.py
+++ Code/main.py
@@ -14,7 +14,7 @@ import aiohttp
 import asyncio
 
 # Version control naj bi bil avtomatski
-bot_version = " v: 3.0.2" 
+bot_version = " v: 3.0.3" 
 # Channel ID v ločeni datoteki
 channelID, channelID_BP, channelID_CM = 0, 0, 0
 # User ID v ločeni datoteki
